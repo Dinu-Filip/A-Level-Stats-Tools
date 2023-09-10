77835ae4c307e8251cd3d3b0c934355907297598
Constantin Filip <dinu.filip.self@gmail.com>
2023-09-10 21:30:55 +0100

    Update distributional_parameters.py

--- HypothesisTests/distributional_parameters.py
+++ HypothesisTests/distributional_parameters.py
@@ -11,9 +11,12 @@ class DistributionHT:
     @staticmethod
     def binomial_cr(params: dict, type_test: str, type_tail=None) -> list:
         num_trials = int(params["num_trials"])
+        print(num_trials)
         prob = float(params["population_param_value"])
+        print(prob)
         sig_level = float(params["sig_level"])
-        if type_test == "one_tailed":
+        print(sig_level)
+        if type_test == "one-tailed":
             if type_tail == "lower":
                 cv = binom.ppf(sig_level, num_trials, prob)
                 if binom.cdf(cv, num_trials, prob) > sig_level:
@@ -35,21 +38,21 @@ class DistributionHT:
 
     @staticmethod
     def normal_cr(params: dict, type_test: str, type_tail=None):
-        mean = float(params["mean"])
+        mean = float(params["population_mean"])
         sd = float(params["sd"])
         N = float(params["N"])
         new_sd = math.sqrt((sd ** 2) / N)
         sig_level = float(params["sig_level"])
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             if type_tail == "lower":
-                cv = norm.ppf(sig_level, loc=mean, scale=new_sd)
+                cv = round(norm.ppf(sig_level, loc=mean, scale=new_sd), 5)
                 return [("-\infty", cv)]
             else:
-                cv = norm.ppf(1 - sig_level, loc=mean, scale=new_sd)
+                cv = round(norm.ppf(1 - sig_level, loc=mean, scale=new_sd), 5)
                 return [(cv, "\infty")]
         else:
-            cv_1 = norm.ppf(sig_level / 2, loc=mean, scale=new_sd)
-            cv_2 = norm.ppf(1 - sig_level / 2, loc=mean, scale=new_sd)
+            cv_1 = round(norm.ppf(sig_level / 2, loc=mean, scale=new_sd), 5)
+            cv_2 = round(norm.ppf(1 - sig_level / 2, loc=mean, scale=new_sd), 5)
             return [("-\infty", cv_1), (cv_2, "\infty")]
 
     @staticmethod
@@ -58,17 +61,17 @@ class DistributionHT:
         prob = float(params["population_param_value"])
         print(type_test)
         print(type_tail)
-        if type_test == "one_tailed" and type_tail == "upper":
+        if type_test == "one-tailed" and type_tail == "upper":
             return round(1 - binom.cdf(float(params["test_stat"]), num_trials, prob), 3)
         else:
             return round(binom.cdf(float(params["test_stat"]), num_trials, prob), 3)
 
     @staticmethod
     def normal_p_value(params, type_test, type_tail):
-        mean = float(params["mean"])
+        mean = float(params["population_mean"])
         sd = float(params["sd"])
         new_sd = math.sqrt((sd ** 2) / float(params["N"]))
-        if type_test == "one_tailed" and type_tail == "upper":
+        if type_test == "one-tailed" and type_tail == "upper":
             return round(1 - norm.cdf(float(params["test_stat"]), loc=mean, scale=new_sd), 6)
         else:
             print(mean)
@@ -80,14 +83,15 @@ class DistributionHT:
         match distribution:
             case "binomial": cr = DistributionHT.binomial_cr(params, type_test, type_tail)
             case "normal": cr = DistributionHT.normal_cr(params, type_test, type_tail)
+        print(cr)
         method_3 = r"We model the random variable $X$ as"
         if distribution == "binomial":
             method_3 += r"X \approx B(#num_trials#, #popuation_param_value#)."
         elif distribution == "normal":
-            method_3 += r"X \approx N(#mean#, \frac{#sd#^2}{N}); note that we divide the population variance by the sample size"
+            method_3 += r"X \approx N(#population_mean#, \frac{#sd#^2}{N}); note that we divide the population variance by the sample size"
 
         step_3 = "3) Calculate the critical region, assuming the null hypothesis is true"
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             if type_tail == "lower":
                 method_3 = r"""The critical value $x$ is such that $P(X \leq x) = #sig_level#$.
 We can find $x$ using the inverse #distribution# function; in this case, $x = """ + str(cr[0][1]) + r"$."
@@ -98,9 +102,10 @@ We can find $x$ using the inverse #distribution# function; in this case, $x = ""
 Since $P(X \geq x) = 1 - P(X \leq x - 1)$ for discrete distributions, we have $P(X \leq x - 1) = """ + str(
                         diff) + "$. Using the inverse #distribution# function, we have $x = " + str(cr[0][0]) + " $."
                 else:
-                    method_3 = r"""The critical value $x$ is such that $P(X \geq x) = #sig_level#$. $Since P(X \geq x) = 1 - P(X \leq x)$ for continuous distributions, we have $P(X \leq x) =""", str(
+                    method_3 = r"""The critical value $x$ is such that $P(X \geq x) = #sig_level#$. Since $P(X \geq x) = 1 - P(X \leq x)$ for continuous distributions, we have $P(X \leq x) = """ + str(
                         diff) + "$. Using the inverse #distribution# function, we have $x = " + str(cr[0][1]) + "$. "
-            method_3 += r"Therefore the critical region is $[" + str(cr[0][0]) + ", " + str(cr[0][1]) + "]$"
+            method_3 += r"""
+Therefore the critical region is $[""" + str(cr[0][0]) + ", " + str(cr[0][1]) + "]$"
         else:
             half_prob = round(float(params["sig_level"]) / 2, 5)
             method_3 = r"The critical values $a$, $b$ on either end of the distribution are such that $P(X \leq a) = " + str(
@@ -121,7 +126,7 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
 
     @staticmethod
     def get_p_value(distribution: str, params: dict, type_test: str, type_tail=None):
-        step_3 = r"Calculate p-value assuming null hypothesis is true"
+        step_3 = r"3) Calculate p-value assuming null hypothesis is true"
         p_val = None
         match distribution:
             case "binomial": p_val = DistributionHT.binomial_p_value(params, type_test, type_tail)
@@ -131,41 +136,41 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
         if distribution == "binomial":
             method_3 += r"X \approx B(#num_trials#, #popuation_param_value#)."
         elif distribution == "normal":
-            method_3 += r"X \approx N(#mean#, \frac{#sd#^2}{N}); note that we divide the population variance by the sample size"
+            method_3 += r"X \approx N(#population_mean#, \frac{#sd#^2}{N}); note that we divide the population variance by the sample size"
 
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             if type_tail == "lower":
                 method_3 = r"Use the #distribution# CDF: P(X \leq #test_stat#) = " + str(p_val)
             else:
                 if distribution in DistributionHT.discrete_distributions:
-                    method_3 = r"Using P(X \geq x) = 1 - P(X \leq x - 1) for discrete distributions, P(X \geq #test_stat#) = " + str(p_val)
+                    method_3 = r"Using $P(X \geq x) = 1 - P(X \leq x - 1)$ for discrete distributions, $P(X \geq #test_stat#) = " + str(p_val) + "$"
                 else:
-                    method_3 = r"Using P(X \geq x) = 1 - P(X \leq x) for continuous distributions, P(X \geq #test_stat#) = " + str(p_val)
+                    method_3 = r"Using $P(X \geq x) = 1 - P(X \leq x)$ for continuous distributions, $P(X \geq #test_stat#) = " + str(p_val) + "$"
         else:
-            method_3 = r"We need to find the p value at both tails, i.e. P(X \leq #test_stat#) and P(X \geq #test_stat#) and compare each to half the significance level."
-            method_3 += r"Using the #distribution# cumulative distribution function, P(X \leq #test_stat#) = " + str(p_val) + "."
+            method_3 = r"We need to find the p value at both tails, i.e. $P(X \leq #test_stat#)$ and $P(X \geq #test_stat#)$ and compare each to half the significance level."
+            method_3 += r"Using the #distribution# cumulative distribution function, $P(X \leq #test_stat#) = " + str(p_val) + "$"
             diff = 1 - p_val
             if distribution in DistributionHT.discrete_distributions:
-                method_3 += r"Using P(X \geq x) = 1 - P(X \leq x - 1) for discrete distributions, P(X \geq x) = " + str(
-                    diff)
+                method_3 += r"Using $P(X \geq x) = 1 - P(X \leq x - 1)$ for discrete distributions, $P(X \geq x) = " + str(
+                    diff) + "$"
             else:
-                method_3 += r"Using P(X \geq x) = 1 - P(X \leq x) for continuous distributions, P(X \geq x) = " + str(
-                    diff)
+                method_3 += r"Using $P(X \geq x) = 1 - P(X \leq x)$ for continuous distributions, $P(X \geq x) = " + str(
+                    diff) + "$"
         return p_val, step_3, method_3
 
     @staticmethod
     def get_hypothesis(type_test: str, type_tail=None):
         step_1 = r"1) State the null hypothesis: $H_0$: $#population_param_name# = #population_param_value#$"
         method_1 = r"Under the null hypothesis, we model the random variable $X$ with a #distribution# distribution"
-        step_2 = r"2) State the alternative hypothesis: $H_1 = "
-        if type_test == "one_tailed":
+        step_2 = r"2) State the alternative hypothesis: $H_1$: "
+        if type_test == "one-tailed":
             if type_tail == "lower":
-                step_2 += r"#population_param_name# < #population_param_value#$"
+                step_2 += r"$#population_param_name# < #population_param_value#$"
             else:
-                step_2 += r"#population_param_name# > #population_param_value#$"
-            method_2 = r"For a one_tailed test, there is a single critical region"
+                step_2 += r"$#population_param_name# > #population_param_value#$"
+            method_2 = r"For a one-tailed test, there is a single critical region"
         else:
-            step_2 += r"#population_param_name# \neq #population_param_value#$"
+            step_2 += r"$#population_param_name# \neq #population_param_value#$"
             method_2 = r"For a two-tailed test, there are two critical regions, one at each end of the distribution representing half the significance level."
         return step_1, method_1, step_2, method_2
 
@@ -173,7 +178,7 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
     def get_test_result_cr(cr, test_stat, type_test, type_tail):
         step_4_CR = "4) Compare test statistic to critical region"
         method_4_CR = r"The test statistic is $#test_stat# "
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             if type_tail == "lower":
                 if test_stat <= cr[0][1]:
                     method_4_CR += r"<= " + str(cr[0][1])
@@ -206,8 +211,8 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
 
     @staticmethod
     def get_test_result_p_val(p_val, sig_level, type_test):
-        step_4 = "Compare to the significance level"
-        if type_test == "one_tailed":
+        step_4 = "4) Compare to the significance level"
+        if type_test == "one-tailed":
             if float(p_val) < float(sig_level):
                 method_4 = r"""The p-value is less than the significance level of #sig_level#, so there is sufficient evidence to suggest that the null hypothesis is false in favour of the alternative hypothesis"""
                 result = False
@@ -231,7 +236,7 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
     def binomial_actual_sig_level(type_test, type_tail, cr, params):
         num_trials = float(params["num_trials"])
         prob = float(params["population_param_value"])
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             if type_tail == "lower":
                 actual_sig_level = round(binom.cdf(cr[0][1], num_trials, prob), 5)
                 return actual_sig_level, "P(X <= " + str(cr[0][1]) + ") = " + str(actual_sig_level)
@@ -241,35 +246,36 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
                 return actual_sig_level, "P(X >= " + str(cr[0][0]) + ") = " + str(actual_sig_level)
         else:
             actual_sig_level = round(
-                binom.cdf(cr[0][1], num_trials, prob) + (1 - binom.cdf(cr[1][0], num_trials, prob)))
-            return actual_sig_level, "P(X <= " + str(cr[0][1]) + ") + P(X >= " + str(cr[1][0]) + ") = " + str(actual_sig_level)
+                binom.cdf(cr[0][1], num_trials, prob) + (1 - binom.cdf(cr[1][0] - 1, num_trials, prob)), 5)
+            return actual_sig_level, "$P(X <= " + str(cr[0][1]) + ") + P(X >= " + str(cr[1][0]) + ") = " + str(actual_sig_level) + "$"
 
     @staticmethod
     def normal_actual_sig_level(type_test, type_tail, cr, params):
-        mean = float(params["mean"])
+        mean = float(params["population_mean"])
         sd = float(params["sd"])
         N = float(params["N"])
         new_sd = math.sqrt((sd ** 2) / N)
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             if type_tail == "lower":
                 actual_sig_level = round(norm.cdf(cr[0][1], loc=mean, scale=new_sd), 5)
-                return actual_sig_level, "P(X <= " + str(cr[0][1]) + ") = " + str(actual_sig_level)
+                return actual_sig_level, "$P(X \leq " + str(cr[0][1]) + ") = " + str(actual_sig_level) + "$"
             else:
                 actual_sig_level = round(1 - norm.cdf(cr[0][0], loc=mean, scale=new_sd), 5)
-                return actual_sig_level, "P(X >= " + str(cr[0][0]) + ") = " + str(actual_sig_level)
+                return actual_sig_level, "$P(X \geq " + str(cr[0][0]) + ") = " + str(actual_sig_level) + "$"
         else:
             actual_sig_level = round(norm.cdf(cr[0][1], loc=mean, scale=new_sd) + 1 - norm.cdf(cr[1][0], loc=mean, scale=new_sd), 5)
-            return actual_sig_level, "P(X <= " + str(cr[0][1]) + ") + P(X >= " + str(cr[1][0]) + ") = " + str(actual_sig_level)
+            return actual_sig_level, "$P(X \leq " + str(cr[0][1]) + ") + P(X >= " + str(cr[1][0]) + ") = " + str(actual_sig_level) + "$"
 
     @staticmethod
     def calculate_actual_sig_level(distribution, type_test, type_tail, cr, params):
         step_6 = "6) Calculate the actual significance level of the test"
         method_6 = "The actual significance level is the probability that the test statistic lies within the critical region assuming the null hypothesis: "
         actual_sig_level = None
+        sig_calc = ""
         match distribution:
             case "binomial": actual_sig_level, sig_calc = DistributionHT.binomial_actual_sig_level(type_test, type_tail, cr, params)
             case "normal": actual_sig_level, sig_calc = DistributionHT.normal_actual_sig_level(type_test, type_tail, cr, params)
-        return actual_sig_level, step_6, method_6
+        return actual_sig_level, step_6, method_6 + sig_calc
 
     @staticmethod
     def distribution(distribution: str, type_test: str, params: dict):
@@ -277,7 +283,7 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
         # Finds which end of distribution to test if one-tailed test
         #
         type_tail = None
-        if type_test == "one_tailed":
+        if type_test == "one-tailed":
             type_tail = params["type_tail"]
         #
         # Gets substituted null and alternative hypotheses
@@ -300,10 +306,16 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
         actual_sig_level, step_6_cr, method_6_cr = DistributionHT.calculate_actual_sig_level(distribution, type_test, type_tail, cr,
                                                                                              params)
         step_5_p, method_5_p = step_6_cr, method_6_cr
+        step_5_p = "5" + step_6_cr[1:]
+        if type_test == "one-tailed":
+            cr_res = f"$[{cr[0][0]}, {cr[0][1]}]$"
+        else:
+            cr_res = fr"$[{cr[0][0]}, {cr[0][1]}] \cup [{cr[1][0]}, {cr[1][1]}]$"
 
-        result = {"res": {"outcome": result,
-                          "critical_region": cr,
-                          "actual_sig_level": actual_sig_level},
+
+        result = {"res": {"outcome": str(result),
+                          "critical_region": cr_res,
+                          "actual_sig_level": str(actual_sig_level)},
                   "critical region": {step_1: method_1,
                                       step_2: method_2,
                                       step_3_CR: method_3_CR,
@@ -326,11 +338,4 @@ To calculate $a$, we can directly use the inverse #distribution# function; this 
 
 
 if __name__ == "__main__":
-    print(DistributionHT.distribution("normal", "two_tailed", {"test_stat": "8.95",
-                                                                 "distribution": "normal",
-                                                                 "population_param_name": r"\mu",
-                                                                 "population_param_value": "9",
-                                                               "mean": "9",
-                                                                 "N": "30",
-                                                                 "sd": "0.15",
-                                                                 "sig_level": "0.05"}))
+    print(DistributionHT.distribution("binomial", "two-tailed", {"num_trials":"10","prob":"0.333","X":"1","dp":"2","sig_level":"0.05","test_stat":"1","distribution":"binomial","population_param_name":"p","population_param_value":"0.333","type_test":"two-tailed"}))
